26867d23f1c9439c57ef8296526e20879b5f26e8
junngo <myeongjun.ko@gmail.com>
2020-07-11 17:57:59 +0900

    added post update

--- src/main/java/org/junngo/spring/springboot/service/PostsService.java
+++ src/main/java/org/junngo/spring/springboot/service/PostsService.java
@@ -1,8 +1,11 @@
 package org.junngo.spring.springboot.service;
 
 import lombok.RequiredArgsConstructor;
+import org.junngo.spring.springboot.domain.posts.Posts;
 import org.junngo.spring.springboot.domain.posts.PostsRepository;
+import org.junngo.spring.springboot.web.dto.PostsResponseDto;
 import org.junngo.spring.springboot.web.dto.PostsSaveRequestDto;
+import org.junngo.spring.springboot.web.dto.PostsUpdateRequestDto;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,5 +17,22 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 }
